7543bf679e138dd3f4e7186c7442e93bb8f7c8a6
AlexCabreraD <109772528+AlexCabreraD@users.noreply.github.com>
2024-10-23 15:15:03 -0600

    added loading skeleton to image preview on modal

--- src/app/components/ModalComponent.tsx
+++ src/app/components/ModalComponent.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BsCircleFill } from "react-icons/bs";
 import { FaLock } from "react-icons/fa";
 import Image from "next/image";
 
 interface ModalProps {
-  imageUrl: string; // URL or file path of the full screenshot
-  onClose: () => void; // Function to close the modal
-  liveSiteUrl?: string; // URL to open the live site
+  imageUrl: string;
+  onClose: () => void;
+  liveSiteUrl?: string;
   headerTitle: string;
   liveUrl: string;
 }
@@ -18,13 +18,15 @@ const ModalComponent: React.FC<ModalProps> = ({
   headerTitle,
   liveUrl,
 }) => {
-  // Disable scroll when modal is open and re-enable when closed
+  const [isImageLoading, setIsImageLoading] = useState(true);
+
   useEffect(() => {
-    document.body.style.overflow = "hidden"; // Disable scrolling
+    document.body.style.overflow = "hidden";
     return () => {
-      document.body.style.overflow = ""; // Reset scroll behavior when modal is closed
+      document.body.style.overflow = "";
     };
   }, []);
+
   return (
     <div
       className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-80 z-50"
@@ -63,25 +65,43 @@ const ModalComponent: React.FC<ModalProps> = ({
           </div>
         </div>
 
-        <div className="h-[70vh] md:h-[90vh] overflow-auto">
-          <div className="flex justify-center w-full">
+        <div className="h-[70vh] md:h-[90vh] overflow-auto flex justify-center">
+          <div className="justify-center w-full">
+            {isImageLoading && (
+              <div className="flex items-center justify-center w-full h-full bg-black rounded animate-pulse">
+                <svg
+                  className="w-10 h-10 text-gray-200"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 18"
+                >
+                  <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+                </svg>
+              </div>
+            )}
             <Image
               src={imageUrl}
               alt="Website Screenshot"
-              className="w-full h-auto object-cover"
+              className={`w-full h-auto object-cover ${
+                isImageLoading ? "hidden" : "block"
+              }`}
               width={1440}
               height={1080}
+              onLoadingComplete={() => setIsImageLoading(false)}
               priority
+              placeholder="blur"
             />
           </div>
         </div>
+
         <button
           className={"bg-[#181818] py-[8px] w-full lg:hidden"}
           onClick={() => {
             onClose();
           }}
         >
-          close
+          Close
         </button>
       </div>
     </div>
